5d29f58a81a08d97ac1493808b61f5c25b7a98f6
gness1804 <graham.nessler@gmail.com>
2016-08-14 16:02:08 -0600

    Added new data and styling

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function activate() {
-
-};
+$(document).ready(function () {
+  $(".scottie").click(function () {
+    $("#frame1").attr("src", "http://www.akc.org/embeds/breed-standard/scottish-terrier");
+  });
+  $(".westie").click(function () {
+    $("#frame1").attr("src", "http://www.akc.org/embeds/breed-standard/west-highland-white-terrier");
+  });
+  $(".cairn").click(function () {
+    $("#frame1").attr("src", "http://www.akc.org/embeds/breed-standard/cairn-terrier");
+  });
+}); //end of jQuery body
